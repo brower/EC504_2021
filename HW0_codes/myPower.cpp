94e2bbd5945c09e99044b5a08347d883aeb19f69
brower <brower@bu.edu>
2021-09-06 23:44:24 -0400

    Added veryfastPower via bit manipulation

--- HW0_codes/myPower.cpp
+++ HW0_codes/myPower.cpp
@@ -1,21 +1,19 @@
-/***********************************************************
-Main progam template for practice exercise HomeworkZero
-
-Fast log(N) for positive powers of double x?
-
-C and C++ is not smart enough to do this!
-************************************************************/
-
-#include <iostream>
+x#include <iostream>
 #include <fstream>
 #include <chrono>
 #include <cstring>
 #include <math.h>
 using namespace std;
 
+/***********************************************************
+Main progam template for practice exercise HomeworkZero
+************************************************************/
+
 double slowPower(double x, long int N);
 double fastPower(double x, long int N);
 double  cPower(double x, long int N);
+double veryfastPower(double x, long int N);
+
 
 int main(int argc, char **argv) 
 {
@@ -37,16 +35,16 @@ int main(int argc, char **argv)
     {printf("Error need two command line args for x and N\n");
       return 0;
     }
-  
+
   /* Timeing and IO setup */
   chrono::time_point<chrono::steady_clock> start, stop; 
   chrono::duration<double> difference_in_time;
   double difference_in_sec_slow; // Holds the final run time
   double difference_in_sec_fast; // Holds the final run time
   double difference_in_sec_c; // Holds the final run time
-  
+  double difference_in_sec_veryfast; // final run time  
 
-    
+ 
   start = chrono::steady_clock::now();
   double power_c = cPower(x,N);
   stop = chrono::steady_clock::now();
@@ -65,9 +63,17 @@ int main(int argc, char **argv)
   difference_in_time = stop - start;
   difference_in_sec_fast= double(difference_in_time.count());
 
+  start = chrono::steady_clock::now();
+  double power_veryfast = veryfastPower(x,N);
+  stop = chrono::steady_clock::now();
+  difference_in_time = stop - start;
+  difference_in_sec_veryfast= double(difference_in_time.count());
+  
   cout << "powerC =    " << power_c   << "  Time = " <<  difference_in_sec_c << endl;
   cout << "powerSlow = " << power_slow <<"  Time = " <<  difference_in_sec_slow << endl;
   cout << "powerFast = " << power_fast <<"  Time = " <<  difference_in_sec_fast << endl;
+  cout << "powerveryFast = " << power_veryfast <<"  Time = " <<  difference_in_sec_veryfast << endl;
+
   
   return 0;
 }
@@ -93,19 +99,34 @@ double slowPower(double x, long int N)
   if(i < N)   cout <<"Slow Failed with iteration stop at i = " << i << endl;
   return pow ;
 }
-
+//This function needs to be fixed!
 double  fastPower(double x, long int N)
 {
   double square = x;
   double pow = 1.0;
   while(N > 0)
-    {  
-      if(N%2)   ;  // {??? }    
-	// ???? ;
-      //     	cout<< " N%2 = "<<  N%2 << " N/2 = "<<  N <<  endl;
+    { 
+      if(N%2) pow *= 1 ; // Update pow 
       N = N/2;
+      // Update square
     }
   return pow;
+}
+
+double veryfastPower(double x, long int N)
+{
+    double pow = 1.0;
+    for (;;)
+    {
+      if (N & 1)  //Copies a bit to the result if it exists in both operands.
+            pow *= x;
+      N >>= 1;  //Binary Right Shift Operator. 
+      if (!N)  // Check for zero
+            break;
+        x *= x;
+    }
+
+    return pow;
 }
 
 
